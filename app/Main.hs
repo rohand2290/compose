2da54b8004cb1cbf7327b19889f117e00aad725b
Rohan Deshpande <rbdeshpande@ucla.edu>
2025-06-17 13:27:14 -0700

    finished parsing functionality in ParseDir

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,24 @@
 import CMark
 import qualified Data.Text.IO
-import ParseDir (parsePost, readDir, BlogDir)
+import ParseDir (parsePost, readBlogArr, BlogDir, Post)
 import System.Directory
+import System.FilePath
 
 
 main :: IO ()
 main = do
  postList <- readDir "blogtest/"
  putStrLn (show postList)  
+
+
+
+readDir :: FilePath -> IO BlogDir
+readDir filepath = do
+                    filepaths <- listDirectory filepath
+		    absFilepaths <- traverse (makeAbsolute . (filepath </>)) filepaths
+		    textList <- traverse Data.Text.IO.readFile absFilepaths
+		    return (readBlogArr textList)
+
  
 
 
--- app/ParseDir.hs
+++ app/ParseDir.hs
@@ -2,7 +2,7 @@ module ParseDir
 (Post (..),
  BlogDir (..),
  parsePost,
- readDir)
+ readBlogArr)
 where
 
 import System.Directory (listDirectory, makeAbsolute)
@@ -10,7 +10,7 @@ import System.FilePath
 
 import CMark
 import qualified Data.Text.IO
-import Data.Text
+import qualified Data.Text
 
 
 
@@ -20,7 +20,7 @@ data Post = Post {
  date :: String,
  title :: String,
  content :: Node,
- tag :: String
+ tag :: Maybe String
 } deriving Show
 
 data BlogDir = BlogDir {
@@ -32,26 +32,26 @@ data BlogDir = BlogDir {
 
 
 
-parsePost :: Text -> Post
+parsePost :: Data.Text.Text -> Post
 parsePost postText = case commonmarkToNode [optSmart] postText of
   (Node _ DOCUMENT (x:xs)) -> case x of
-   (Node _ (HEADING 1) [Node _ (TEXT t) _]) -> if t == (pack "index")
-    then Post True "today" "index" (commonmarkToNode [optSmart] postText) "na"
-    else Post False "today" (unpack t) (commonmarkToNode [optSmart] postText) "na"
-   _ -> Post False "today" "First Post" (commonmarkToNode [optSmart] postText) "na"
-  _ -> Post False "today" "First Post" (commonmarkToNode [optSmart] postText) "na" 
+   (Node _ (HEADING 1) [Node _ (TEXT t) _]) -> if t == (Data.Text.pack "index")
+    then Post True "today" "index" (commonmarkToNode [optSmart] postText) Nothing
+    else Post False "today" (Data.Text.unpack t) (commonmarkToNode [optSmart] postText) Nothing
+   _ -> Post False "today" "First Post" (commonmarkToNode [optSmart] postText) Nothing
+  _ -> Post False "today" "First Post" (commonmarkToNode [optSmart] postText) Nothing
 
 
 
 
 
 
-readDir :: FilePath -> IO [Post]
-readDir filepath = do
-                    filepaths <- listDirectory filepath
-		    absFilepaths <- traverse (makeAbsolute . (filepath </>)) filepaths
-		    textList <- traverse Data.Text.IO.readFile absFilepaths
-		    return (fmap parsePost textList)
+
+readBlogArr :: [Data.Text.Text] -> BlogDir
+readBlogArr arr = BlogDir {
+ index = head (filter (\post -> isIndex post == True) (fmap parsePost arr)), 
+ posts = (filter (\post -> isIndex post == True) (fmap parsePost arr))
+}
 		    
 		    
 
